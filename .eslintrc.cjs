a20ff2f3acd78266a8c2da5ccfcc6e0ac910f99b
artem_alekseenko <ivanovo.cwi@gmail.com>
2023-05-06 20:56:18 +0300

    initial commit

---
+++ .eslintrc.cjs
@@ -0,0 +1,25 @@
+/* eslint-env node */
+module.exports = {
+  root: true,
+  extends: [
+    'airbnb-base',
+    'plugin:vue/vue3-essential',
+    'eslint:recommended',
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+  },
+  settings: {
+    'import/resolver': {
+      alias: {
+        map: [['@', './src']],
+        extensions: ['.js', '.jsx', '.ts', '.tsx', '.vue'],
+      },
+    },
+  },
+  ignorePatterns: ['vitest.config.js'],
+  rules: {
+    'max-len': ['error', { code: 120 }],
+    'import/prefer-default-export': 'off',
+  },
+};
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,15 @@
+<script setup>
+defineProps({
+  msg: {
+    type: String,
+    default: 'Hello World!',
+  },
+});
+</script>
+
+<template>
+    <h1>{{ msg }}</h1>
+</template>
+
+<style scoped>
+</style>
