eda75df7a019c15214dee4dfc9de7e673fddd2d9
artem_alekseenko <ivanovo.cwi@gmail.com>
2023-05-07 21:33:11 +0300

    feat: add UserProfileCard cmp

--- src/stores/UsersStore.js
+++ src/stores/UsersStore.js
@@ -1,11 +1,31 @@
-import { reactive } from 'vue';
+import { computed, reactive, ref } from 'vue';
 import { defineStore } from 'pinia';
 import sourceData from '@/data.json';
+// eslint-disable-next-line import/no-cycle
+import { usePostsStore } from '@/stores/PostsStore';
+import { useThreadsStore } from '@/stores/ThreadsStore';
 
 export const useUsersStore = defineStore('UsersStore', () => {
   const users = reactive(sourceData.users);
+  const authUserId = ref(users[1].id);
 
-  const authUser = reactive(users[1]);
+  const authUser = computed(() => {
+    const user = users.find((u) => u.id === authUserId.value);
+    if (!user) {
+      return null;
+    }
+
+    const posts = usePostsStore().getPostsByUserId(user.id);
+    const threads = useThreadsStore().getThreadsByUserId(user.id);
+
+    return {
+      ...user,
+      posts,
+      postsCount: posts.length,
+      threads,
+      threadsCount: threads.length,
+    };
+  });
 
   const getUserById = (id) => users.find((u) => u.id === id);
 
--- src/views/ProfilePage.vue
+++ src/views/ProfilePage.vue
@@ -2,53 +2,16 @@
 import PostList from '@/components/PostList.vue';
 import { useUsersStore } from '@/stores/UsersStore';
 import { storeToRefs } from 'pinia';
-import { usePostsStore } from '@/stores/PostsStore';
-import { computed } from 'vue';
-import { useThreadsStore } from '@/stores/ThreadsStore';
+import UserProfileCard from '@/components/UserProfileCard.vue';
 
-const postsStore = usePostsStore();
 const { authUser: user } = storeToRefs(useUsersStore());
-const userPosts = computed(() => postsStore.getPostsByUserId(user.value.id));
-const userPostsCount = computed(() => userPosts.value.length);
-
-const threadsStore = useThreadsStore();
-const userThreads = computed(() => threadsStore.getThreadsByUserId(user.value.id));
-const userThreadsCount = computed(() => userThreads.value.length);
 </script>
 
 <template>
   <div class="container">
     <div class="flex-grid">
       <div class="col-3 push-top">
-        <div class="profile-card">
-          <p class="text-center">
-            <img
-                :src="user.avatar"
-                :alt="`${user.name} profile picture`"
-                class="avatar-xlarge"
-            />
-          </p>
-
-          <h1 class="title">{{ user.username }}</h1>
-
-          <p class="text-lead">{{ user.name }}</p>
-
-          <p class="text-justify">{{ user.bio || "No bio specified." }}</p>
-
-          <span class="online">{{ user.username }} is online</span>
-
-          <div class="stats">
-            <span>{{ userPostsCount }} posts</span>
-            <span>{{ userThreadsCount }} threads</span>
-          </div>
-
-          <hr />
-
-          <p v-if="user.website" class="text-large text-center">
-            <i class="fa fa-globe"></i>
-            <a :href="user.website">{{ user.website }}</a>
-          </p>
-        </div>
+        <UserProfileCard :user="user" />
 
         <p class="text-xsmall text-faded text-center">
           Member since june 2003, last visited 4 hours ago
@@ -68,7 +31,7 @@ const userThreadsCount = computed(() => userThreads.value.length);
           <a href="#">See only started threads?</a>
         </div>
         <hr />
-        <PostList :posts="userPosts" />
+        <PostList :posts="user.posts" />
       </div>
     </div>
   </div>
