941fc9320fb828ce0f8e7a56da5703c75589bd11
aalekseenko <a.alekseenko@amarkets.com>
2023-05-11 21:47:56 +0300

    chore: update deps

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,18 @@
 import { fileURLToPath, URL } from 'node:url';
-
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
+import eslint from 'vite-plugin-eslint';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    eslint({
+      lintOnStart: true,
+      emitError: true,
+      emitWarning: true,
+      failOnError: true,
+    })],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
