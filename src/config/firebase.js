a6ea55c9f50c850d3dabdc40589c877ae9160a42
aalekseenko <a.alekseenko@amarkets.com>
2023-05-17 23:57:15 +0300

    feat: add firebase store

---
+++ src/config/firebase.js
@@ -0,0 +1,12 @@
+import { initializeApp } from 'firebase/app';
+
+const config = {
+  apiKey: 'AIzaSyAg58DlC0B_XJAUH5ukgdvC1q0XIZ6M4i0',
+  authDomain: 'vue3-forum-7688c.firebaseapp.com',
+  projectId: 'vue3-forum-7688c',
+  storageBucket: 'vue3-forum-7688c.appspot.com',
+  messagingSenderId: '909807157313',
+  appId: '1:909807157313:web:65a6db13258ebaa0f046c0',
+};
+
+export const firebaseApp = initializeApp(config);
