80d5d8673ada993fc8ff6b2205160ef0f2070e22
aalekseenko <a.alekseenko@amarkets.com>
2023-05-27 21:36:38 +0300

    feat(forum): add getting data from firebase in ForumPage #PRO-1

--- src/views/ForumPage.vue
+++ src/views/ForumPage.vue
@@ -2,6 +2,7 @@
 import ThreadList from '@/components/ThreadList.vue';
 import { useForumsStore } from '@/stores/ForumsStore';
 import { useThreadsStore } from '@/stores/ThreadsStore';
+import { ref, watchEffect } from 'vue';
 
 const props = defineProps({
   id: {
@@ -10,30 +11,51 @@ const props = defineProps({
   },
 });
 
+const forum = ref(null);
 const forumsStore = useForumsStore();
-const forum = forumsStore.forum(props.id);
+const forumPromise = forumsStore.forum(props.id);
 
 const threadsStore = useThreadsStore();
-const threads = forum.threadIds.map((threadId) => threadsStore.thread(threadId));
+const threads = ref([]);
+
+watchEffect(async () => {
+  forum.value = await forumPromise;
+});
+
+watchEffect(async () => {
+  if (forum.value?.threadIds) {
+    const threadPromises = await forum.value.threadIds.map((threadId) => threadsStore.thread(threadId));
+    threads.value = await Promise.all(threadPromises);
+  }
+});
+
 </script>
 
 <template>
-  <div class="col-full push-top">
-    <div class="forum-header">
-      <div class="forum-details">
-        <h1>{{ forum.name }}</h1>
-        <p class="text-lead">{{ forum.description }}</p>
+  <div
+     v-if="!forum || !threads"
+     class="text-center"
+  >
+    Loading...
+  </div>
+  <template v-if="forum && threads">
+    <div class="col-full push-top">
+      <div class="forum-header">
+        <div class="forum-details">
+          <h1>{{ forum.name }}</h1>
+          <p class="text-lead">{{ forum.description }}</p>
+        </div>
+        <router-link
+            :to="{name:'ThreadCreate', params: {forumId: forum.id}}"
+            class="btn-green btn-small"
+        >
+          Start a thread
+        </router-link>
       </div>
-      <router-link
-          :to="{name:'ThreadCreate', params: {forumId: forum.id}}"
-          class="btn-green btn-small"
-      >
-        Start a thread
-      </router-link>
     </div>
-  </div>
 
-  <div class="col-full push-top">
-    <ThreadList :threads="threads"/>
-  </div>
+    <div class="col-full push-top">
+      <ThreadList :threads="threads"/>
+    </div>
+  </template>
 </template>
