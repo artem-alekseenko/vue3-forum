5cc5f76df47f42a787fce148e668cd7c28b2a4e4
aalekseenko <a.alekseenko@amarkets.com>
2023-05-27 21:35:45 +0300

    feat(store): add fetchForum from firebase in ForumsStore #PRO-1

--- src/stores/ForumsStore.js
+++ src/stores/ForumsStore.js
@@ -2,20 +2,36 @@ import { reactive } from 'vue';
 import { defineStore } from 'pinia';
 import sourceData from '@/data.json';
 import { findById, makeAppendChildToParent } from '@/helpers';
+import { db } from '@/config/firebase';
+import { doc, getDoc } from 'firebase/firestore';
 
 export const useForumsStore = defineStore('ForumsStore', () => {
   const forums = reactive(sourceData.forums);
 
   const getForumById = (id) => findById(forums, id);
 
-  const forum = (id) => {
-    // eslint-disable-next-line no-shadow
-    const forum = findById(forums, id);
+  const fetchForum = async (id) => {
+    const forumDocRef = doc(db, 'forums', id);
+    const forumDocSnap = await getDoc(forumDocRef);
+    if (forumDocSnap.exists()) {
+      const res = forumDocSnap.data();
+      return {
+        id: forumDocSnap.id,
+        ...res,
+      };
+    }
+    // eslint-disable-next-line no-console
+    console.error(`No forum with id ${id}`);
+    return null;
+  };
+
+  const forum = async (id) => {
+    const forumFromDb = await fetchForum(id);
 
     return {
-      ...forum,
+      ...forumFromDb,
       get threadIds() {
-        return forum.threads || [];
+        return forumFromDb.threads || [];
       },
     };
   };
