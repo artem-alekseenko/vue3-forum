58680a3dfe65d628ddfff3c70cd20673fdaec4d3
aalekseenko <a.alekseenko@amarkets.com>
2023-05-27 21:58:10 +0300

    feat(category): add fetch data from firebase in CategoriesStore #PRO-1

--- src/stores/CategoriesStore.js
+++ src/stores/CategoriesStore.js
@@ -1,12 +1,26 @@
 import { reactive } from 'vue';
 import { defineStore } from 'pinia';
 import sourceData from '@/data.json';
-import { findById } from '@/helpers';
+import { doc, getDoc } from 'firebase/firestore';
+import { db } from '@/config/firebase';
 
 export const useCategoriesStore = defineStore('CategoriesStore', () => {
   const categories = reactive(sourceData.categories);
 
-  const getCategoryById = (id) => findById(categories, id);
+  const getCategoryById = async (id) => {
+    const categoryDocRef = doc(db, 'categories', id);
+    const categoryDocSnap = await getDoc(categoryDocRef);
+    if (categoryDocSnap.exists()) {
+      const res = categoryDocSnap.data();
+      return {
+        id: categoryDocSnap.id,
+        ...res,
+      };
+    }
+    // eslint-disable-next-line no-console
+    console.error(`No category with id ${id}`);
+    return null;
+  };
 
   return { categories, getCategoryById };
 });
