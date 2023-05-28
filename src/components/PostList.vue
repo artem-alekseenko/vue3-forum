c2f1528c16d732ee7507e6182640df97d85057e1
aalekseenko <a.alekseenko@amarkets.com>
2023-05-28 18:45:18 +0300

    feat(profile): change getting info about user in PostList #PRO-1

--- src/components/PostList.vue
+++ src/components/PostList.vue
@@ -1,5 +1,6 @@
 <script setup>
 import { useUsersStore } from '@/stores/UsersStore';
+import { ref, watchEffect } from 'vue';
 
 const props = defineProps({
   posts: {
@@ -9,7 +10,16 @@ const props = defineProps({
 });
 
 const usersStore = useUsersStore();
-const userById = (userId) => usersStore.user(userId);
+const users = ref({});
+watchEffect(async () => {
+  await Promise.all(
+    props.posts.map(async (post) => {
+      if (!users.value[post.userId]) {
+        users.value[post.userId] = await usersStore.user(post.userId);
+      }
+    }),
+  );
+});
 </script>
 
 <template>
@@ -18,36 +28,34 @@ const userById = (userId) => usersStore.user(userId);
          v-for="post in props.posts"
          :key="post.id"
     >
+      <template v-if="post && users[post.userId]">
+        <div class="user-info">
+          <a href="#" class="user-name">{{users[post.userId].name}}</a>
 
-      <div class="user-info">
-        <a href="#" class="user-name">{{userById(post.userId).name}}</a>
+          <a href="#">
+            <img
+                class="avatar-large" :src="users[post.userId].avatar" alt="">
+          </a>
 
-        <a href="#">
-          <img class="avatar-large" :src="userById(post.userId).avatar" alt="">
-        </a>
+          <p class="desktop-only text-small">{{users[post.userId].postsCount}} posts</p>
+          <p class="desktop-only text-small">{{users[post.userId].threadsCount}} threads</p>
 
-        <p class="desktop-only text-small">{{userById(post.userId).postsCount}} posts</p>
-        <p class="desktop-only text-small">{{userById(post.userId).threadsCount}} threads</p>
-
-      </div>
-
-      <div class="post-content">
-        <div>
-          <p>
-            {{post.text}}
-          </p>
         </div>
-      </div>
 
-      <div class="post-date text-faded">
-        <AppDate :timestamp="post.publishedAt" />
-      </div>
+        <div class="post-content">
+          <div>
+            <p>
+              {{post.text}}
+            </p>
+          </div>
+        </div>
+
+        <div class="post-date text-faded">
+          <AppDate :timestamp="post.publishedAt" />
+        </div>
+      </template>
 
     </div>
 
   </div>
 </template>
-
-<style scoped>
-
-</style>
