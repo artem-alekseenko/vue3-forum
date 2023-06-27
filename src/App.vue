e549c0ae0d92e9c229cbd5871a4318937841ac69
aalekseenko <a.alekseenko@amarkets.com>
2023-06-27 21:21:48 +0300

    feat(components): add NProgress

--- src/App.vue
+++ src/App.vue
@@ -2,13 +2,25 @@
 import TheNavbar from '@/components/TheNavbar.vue';
 import { onBeforeMount, ref } from 'vue';
 import { useRouter } from 'vue-router';
+import NProgress from 'nprogress';
 
 const isReady = ref(false);
 const router = useRouter();
 
+const onPageReady = () => {
+  isReady.value = true;
+  NProgress.done();
+};
+
 onBeforeMount(() => {
+  NProgress.configure({
+    speed: 200,
+    showSpinner: false,
+  });
+
   router.beforeEach(() => {
     isReady.value = false;
+    NProgress.start();
   });
 });
 </script>
@@ -18,8 +30,15 @@ onBeforeMount(() => {
   <div class="container">
     <router-view
       v-show="isReady"
-      @ready="isReady = true"
+      @ready="onPageReady"
     />
     <AppSpinner v-show="!isReady" />
   </div>
 </template>
+
+<style>
+  @import "nprogress/nprogress.css";
+  #nprogress .bar{
+    background: #24fe8c !important;
+  }
+</style>
